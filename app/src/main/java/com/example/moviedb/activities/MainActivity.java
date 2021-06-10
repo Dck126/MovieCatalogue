f9034fd418eea2bdee7c960f998e9c29bca2c696
Dck126 <dickykyky126@gmail.com>
2021-06-10 22:14:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/moviedb/activities/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.moviedb.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.Adapter;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.moviedb.fragment.FragmentFavorite;
+import com.example.moviedb.fragment.FragmentMovie;
+import com.example.moviedb.fragment.FragmentTV;
+import com.example.moviedb.R;
+import com.example.moviedb.utils.BottomBarBehavior;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment = null;
+//    RecyclerView recyclerView;
+//    Boolean isScrolling = false;
+//    Adapter adapter;
+//    int currentItems,totalItems,scrollOutItems;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+//    //infinitifScrolling
+//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//                if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+//                {
+//                    isScrolling = true;
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//                currentItems = manager.getChildCount();
+//                totalItems = manager.getItemsCount();
+//                scrollOutItems = manager.findFirstVisibleItemPosition();
+//
+//                if(isScrolling && (currentItems + scrollOutItems == totalItems))
+//                {
+//                    isScrolling = false;
+//                    fetchData();
+//                }
+//            }
+//        });
+        BubbleNavigationLinearView navigationBar = findViewById(R.id.navigationBar);
+
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigationBar.getLayoutParams();
+        layoutParams.setBehavior(new BottomBarBehavior());
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FragmentMovie()).commit();
+
+        navigationBar.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                switch (position) {
+                    case 0:
+                        fragment = new FragmentMovie();
+                        break;
+                    case 1:
+                        fragment = new FragmentTV();
+                        break;
+                    case 2:
+                        fragment = new FragmentFavorite();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+            }
+        });
+    }
+
+//    private void fetchData() {
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i=0;i<5;i++)
+//                {
+//                    list.add(Math.floor(Math.random()*100)+"");
+//                    adapter.notifyDataSetChanged();
+//                }
+//
+//            }
+//        },5000);
+//    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window window = activity.getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        window.setAttributes(winParams);
+    }
+}
